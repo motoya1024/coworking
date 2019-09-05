f6c88b5a2fb4e666cf020c1a69f783ed5af54704
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-09-05 06:34:24 +0000

    ユーザー一覧修正＆ユーザー編集作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,16 @@ User.create!(name: "わい管理者や！",
              password: "password",
              password_confirmation: "password",
              admin: true)
-             
-User.create!(name: "わいサンプルさん",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password")
 
 Operation.create!(seat: 5,
                   all_seat: 30)
+                  
+60.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
