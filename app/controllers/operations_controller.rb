16b5701811f52ae087e519e767f5f519a2397f89
nozomusato <nojyamaru.pikkoro.morori@gmail.com>
2019-09-20 13:53:47 +0000

    20190920

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -8,7 +8,7 @@ class OperationsController < ApplicationController
   def update
     @operation = Operation.find(1)
     if @operation.update_attributes(operation_params)
-      flash[:success] = "ユーザー情報を更新しました。"
+      flash[:success] = "利用席数を#{@operation.seat}席に変更しました。"
       redirect_to operations_edit_path
     else
       render operations_edit_path
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,7 @@
 class Operation < ApplicationRecord
-    operation = Operation.new(seat:5)
+    validates :seat, presence: true
+    validates :seat, :numericality => {
+              :greater_than_or_equal_to => 0,
+              :less_than_or_equal_to => 30
+           }
 end
