e198cf169475ab03d1cdff3b3fc31f80d277b039
motoyaabe <abetty9750@yahoo.co.jp>
2019-09-29 17:50:53 +0000

    reservation_week_status

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,22 @@
 module ReservationsHelper
+
+  def get_color(week_day)
+    if week_day == Date.current
+      return "week_today_color"
+    elsif week_day.saturday? 
+      return "text-primary"
+    elsif week_day.sunday?
+      return "text-danger"
+    end
+  end
+  
+  def get_height(start_time,end_time)
+    time = ((end_time - start_time)/60/60)
+    (time/24.to_f*1776)
+  end
+  
+  def get_position(start_time,end_time)
+    time = ((end_time - start_time)/60/60)
+    (time/24.to_f*1776)+22
+  end
 end
