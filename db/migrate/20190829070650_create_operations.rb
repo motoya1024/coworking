746535f06d9391986c9d08256514ee89567f8e10
nozomusato <nojyamaru.pikkoro.morori@gmail.com>
2019-10-26 12:06:47 +0000

    20191025コミット

--- db/migrate/20190829070650_create_operations.rb
+++ db/migrate/20190829070650_create_operations.rb
@@ -1,7 +1,7 @@
 class CreateOperations < ActiveRecord::Migration[5.1]
   def change
     create_table :operations do |t|
-      t.integer :all_seat, :limit => 1000
+      t.integer :all_seat, :limit => 100
       t.integer :seat
 
       t.timestamps
